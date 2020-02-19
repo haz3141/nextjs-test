223d35431a7610099fb373ecc441e8dc38e7bb65
Hazael Dominguez <hazaeldominguez@Hazaels-Work-MacBook-Pro.local>
2020-02-19 14:02:12 -0500

    Created Layout and Page component

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,23 @@
 import Link from 'next/link';
-import Header from '../components/Header';
+import Layout from '../components/MyLayout';
 
-export default function Index() {
-return (
-	<div>
-		<Header />
-		<p>Hello Next.js</p>
-	</div>
+const PostLink = props => (
+	<li>
+		<Link href={`/post?title=${props.title}`}>
+			<a>{props.title}</a>
+		</Link>
+	</li>
 );
+
+export default function Blog() {
+	return (
+		<Layout>
+			<h1>My Blog</h1>
+			<ul>
+				<PostLink title='Hello Next.js' />
+				<PostLink title='Learn Next.js is awesome' />
+				<PostLink title='Deploy apps with Zeit' />
+			</ul>
+		</Layout>
+	);
 }
